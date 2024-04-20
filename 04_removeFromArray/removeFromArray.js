3274b0e864c6a1a04840409756128ae26472b7c5
MichalB <marchewa626@gmail.com>
2024-04-20 21:07:46 +0200

    all passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...number) {
+  let array = [];
+  let halfBeforeTheUnwantedElement = array.slice(0, ...number);
+  let halfAfterTheUnwantedElement = array(...(number + 1));
+  let copyWithoutNumber = halfBeforeTheUnwantedElement.concat(halfAfterTheUnwantedElement)
+ return copyWithoutNumber
 };
+
+// const arrayOfLetters = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+// const halfBeforeTheUnwantedElement = arrayOfLetters.slice(0, 2)
+
+// const halfAfterTheUnwantedElement = arrayOfLetters(3);
+
+// const copyWithoutThirdElement = halfBeforeTheUnwantedElement.concat(halfAfterTheUnwantedElement);
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
